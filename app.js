8a5232f7255f3a05d515a853f3c4c30def47ac1b
PJ Jones <pj.jones4000.com>
2022-02-03 10:12:34 -0600

    got it to highlight and return to orig color when key is pressed

--- app.js
+++ app.js
@@ -3,61 +3,71 @@ $(function () {
 
   //hide uppercase keyboard
   $("#keyboard-upper-container").hide();
-
 });
 
-  // show/hide diff keyboards by pressing shift
-  $("body").on({
-    keydown: function (e) {
-      if (e.originalEvent.key === "Shift") showUpper();
-    },
-    keyup: function (e) {
-      if (e.originalEvent.key === "Shift") hideUpper();
-    },
-    // keypress: function() {
-    //   console.log('keypress triggered')
-    // }
-  });
+// show/hide diff keyboards by pressing shift
+$("body").on({
+  keydown: function (e) {
+    if (e.originalEvent.key === "Shift") showUpper();
+  },
+  keyup: function (e) {
+    if (e.originalEvent.key === "Shift") hideUpper();
+  },
+  // keypress: function() {
+  //   console.log('keypress triggered')
+  // }
+});
 
-  function showUpper() {
-    // need shift key pressed
-    $("#keyboard-upper-container").show();
-    $("#keyboard-lower-container").hide();
+function showUpper() {
+  // need shift key pressed
+  $("#keyboard-upper-container").show();
+  $("#keyboard-lower-container").hide();
+}
+
+function hideUpper() {
+  // need shift key released
+  $("#keyboard-upper-container").hide();
+  $("#keyboard-lower-container").show();
+}
+
+// change bg color of key
+$(document).keypress((e) => {
+  $(`#${e.keyCode}`).css("backgroundColor", "yellow");
+
+  {
+    setTimeout(function () {
+      $(`#${e.keyCode}`).css("background-color", "#f5f5f5");
+    }, 300);
   }
+});
 
-  function hideUpper() {
-    // need shift key released
-    $("#keyboard-upper-container").hide();
-    $("#keyboard-lower-container").show();
-  }
+// $("body").on({
+//   // keydown: function (e) {
+//   //   if (e.originalEvent.key === String.fromCharCode(event.which)); highlightKey();
+//   //   console.log(String.fromCharCode(event.which));
+//   // },
+//   keypress: function (e) {
+//     if (e.originalEvent.key === String.fromCharCode(event.which)); highlightKey();
+//     console.log(String.fromCharCode(event.which));
+//   },
+//   keyup: function (e) {
+//     if (e.originalEvent.key === String.fromCharCode(event.which)); unhighlightKey();
+//     console.log(event.which);
+//   }
 
-  // change bg color of key
-  $("body").on({
-    keydown: function (e) {
-      if (e.originalEvent.key === String.fromCharCode(event.which)); highlightKey();
-      console.log(String.fromCharCode(event.which));
-    },
-    keyup: function (e) {
-      if (e.originalEvent.key === String.fromCharCode(event.which)); unhighlightKey();
-      console.log(event.which);
-    },
-    // keypress: function() {
-    //   console.log('keypress triggered')
-    // }
-  });
+// });
 
-  function highlightKey() {
-    // need shift key pressed
-    // $("#keyboard-upper-container").show();
-    // $("#keyboard-lower-container").hide();
-    console.log(document.getElementById(event.which));
-    $(`#${event.which}`).css("background-color", "yellow");
-  }
+// function highlightKey() {
+//   // need shift key pressed
+//   // $("#keyboard-upper-container").show();
+//   // $("#keyboard-lower-container").hide();
+//   // console.log(document.getElementById(event.which));
+//   $(`#${e.keyCode}`).css("background-color", "yellow");
+// }
 
-  function unhighlightKey() {
-    // need shift key released
-    // $("#keyboard-upper-container").hide();
-    // $("#keyboard-lower-container").show();
-    $(`#${event.which}`).css("background-color", "#f5f5f5");
-  }
-
+// function unhighlightKey() {
+//   // need shift key released
+//   // $("#keyboard-upper-container").hide();
+//   // $("#keyboard-lower-container").show();
+//   $(`#${event.which}`).css("background-color", "#f5f5f5");
+// }
