0f240da5510e33d3652f7ce59218c9289fc63820
PJ Jones <pj.jones4000.com>
2022-02-03 09:22:18 -0600

    having trouble getting the lowercase ascii char to work, can highlight uppercase

--- app.js
+++ app.js
@@ -2,34 +2,62 @@ $(function () {
   // Handler for .ready() called.
 
   //hide uppercase keyboard
-$("#keyboard-upper-container").hide();
-
-// show/hide diff keyboards by pressing shift
-$("body").on({
-  keydown: function(e) {
-    if (e.originalEvent.key === "Shift")
-      showUpper();
-  },
-  keyup: function(e) {
-    if (e.originalEvent.key === "Shift")
-      hideUpper();
-  },
-  keypress: function() {
-    console.log('keypress triggered')
-  }
-})
-
-function showUpper() {
-  // need shift key pressed
-  $("#keyboard-upper-container").show();
-  $("#keyboard-lower-container").hide();
-}
-
-function hideUpper() {
-  // need shift key released
   $("#keyboard-upper-container").hide();
-  $("#keyboard-lower-container").show();
-
-}
 
 });
+
+  // show/hide diff keyboards by pressing shift
+  $("body").on({
+    keydown: function (e) {
+      if (e.originalEvent.key === "Shift") showUpper();
+    },
+    keyup: function (e) {
+      if (e.originalEvent.key === "Shift") hideUpper();
+    },
+    // keypress: function() {
+    //   console.log('keypress triggered')
+    // }
+  });
+
+  function showUpper() {
+    // need shift key pressed
+    $("#keyboard-upper-container").show();
+    $("#keyboard-lower-container").hide();
+  }
+
+  function hideUpper() {
+    // need shift key released
+    $("#keyboard-upper-container").hide();
+    $("#keyboard-lower-container").show();
+  }
+
+  // change bg color of key
+  $("body").on({
+    keydown: function (e) {
+      if (e.originalEvent.key === String.fromCharCode(event.which)); highlightKey();
+      console.log(String.fromCharCode(event.which));
+    },
+    keyup: function (e) {
+      if (e.originalEvent.key === String.fromCharCode(event.which)); unhighlightKey();
+      console.log(event.which);
+    },
+    // keypress: function() {
+    //   console.log('keypress triggered')
+    // }
+  });
+
+  function highlightKey() {
+    // need shift key pressed
+    // $("#keyboard-upper-container").show();
+    // $("#keyboard-lower-container").hide();
+    console.log(document.getElementById(event.which));
+    $(`#${event.which}`).css("background-color", "yellow");
+  }
+
+  function unhighlightKey() {
+    // need shift key released
+    // $("#keyboard-upper-container").hide();
+    // $("#keyboard-lower-container").show();
+    $(`#${event.which}`).css("background-color", "#f5f5f5");
+  }
+
