0d816ec757b4346ea5656f35c08adeaabd45b33a
PJ Jones <pj.jones4000.com>
2022-02-04 08:50:21 -0600

    everything working, the wpm calculation is sus though

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function hideUpper() {
 $("#reset").hide();
 
 //timer stuff
-// $("#timer").on("click", function () {
+
 let seconds = 0;
 let el = document.getElementById("seconds-counter");
 setInterval(incrementSeconds, 1000);
@@ -40,8 +40,6 @@ function incrementSeconds() {
   seconds += 1;
   el.innerText = "You have been playing for " + seconds + " seconds.";
 }
-
-// });
 
 // change bg color of key
 $(document).keypress((e) => {
@@ -71,32 +69,19 @@ $("#target-letter").html(sentences[i][j]);
 let mistakes = [];
 
 $(document).keypress((e) => {
-  console.log(e.which);
-  console.log(e.keyCode);
-  console.log(
-    String.fromCharCode(
-      85 <= e.keyCode && e.keyCode <= 95 ? e.keyCode - 32 : e.keyCode
-    )
-  );
-  console.log(sentences[i][j]);
-
   if (
     String.fromCharCode(
       85 <= e.keyCode && e.keyCode <= 95 ? e.keyCode - 32 : e.keyCode
     ) === sentences[i][j]
   ) {
-    console.log("yes");
     $("#feedback").append('<span class="glyphicon glyphicon-ok"></span>');
   } else {
-    console.log("no");
     $("#feedback").append('<span class="glyphicon glyphicon-remove"></span>');
     mistakes.push("I");
-    console.log(mistakes);
   }
   j++;
   $("#target-letter").html(sentences[i][j]);
   $("#yellow-block").css("left", `${(j + 2) * 17}px`);
-  console.log(sentences[i][j]);
 
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds - minutes * 60;
@@ -106,7 +91,6 @@ $(document).keypress((e) => {
   if (sentences[i][j] == undefined) {
     i++;
     if (sentences[i] == undefined) {
-      console.log(sentences[i]);
       let end = document.getElementById("seconds-end");
       $("#seconds-counter").html("");
       end.innerText =
@@ -125,15 +109,10 @@ $(document).keypress((e) => {
         $("#target-letter").html(sentences[i][j]);
         $("#yellow-block").css("left", `${(j + 2) * 17}px`);
         $("#feedback").html("");
-        end.innerText = '';
+        end.innerText = "";
         mistakes = [];
         $("#reset").hide();
       });
-
-      console.log(minutes);
-      console.log(remainingSeconds);
-      console.log(remainingSeconds / 60);
-      console.log(mistakes.length);
     }
 
     j = 0;
@@ -141,6 +120,5 @@ $(document).keypress((e) => {
     $("#target-letter").html(sentences[i][j]);
     $("#yellow-block").css("left", `${(j + 2) * 17}px`);
     $("#feedback").html("");
-    console.log("new sentence");
   }
 });
